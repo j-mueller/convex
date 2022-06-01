cd9bd6c408c0a115c390631c4e7dd2a4a5e5fe37
Jann Müller <j-mueller@users.noreply.github.com>
2022-06-01 15:05:57 +0200

    Work on wallet

--- lib/convex-app-wallet/src/Convex/Wallet/Types.hs
+++ lib/convex-app-wallet/src/Convex/Wallet/Types.hs
@@ -1,12 +1,19 @@
-{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Convex.Wallet.Types(
   Wallet(..),
+  WalletInfo(..),
   address,
+  info,
+  privateKey,
   paymentCredential
   ) where
 
 import Cardano.Api (Address, NetworkId, PaymentCredential, PaymentKey, ShelleyAddr, SigningKey)
 import Cardano.Api qualified as C
+import Data.Aeson (ToJSON (..), object, (.=))
+import Data.Text (Text)
+import Prettyprinter (Pretty (..), vsep, (<+>))
 
 data Wallet =
   Wallet
@@ -26,3 +33,37 @@ paymentCredential Wallet{wKey} =
 address :: NetworkId -> Wallet -> Address ShelleyAddr
 address networkId wallet =
   C.makeShelleyAddress networkId (paymentCredential wallet) C.NoStakeAddress
+
+{-| The wallet's private key (serialised)
+-}
+privateKey :: Wallet -> Text
+privateKey Wallet{wKey} = C.serialiseToBech32 wKey
+
+info :: NetworkId -> Wallet -> WalletInfo
+info networkId w@Wallet{wNonAdaReturnAddress} =
+  WalletInfo
+    { wiReturnAddress = C.serialiseToBech32 wNonAdaReturnAddress
+    , wiAddress       = C.serialiseToBech32 (address networkId w)
+    }
+
+{-| Information about a wallet with serialised addresses
+-}
+data WalletInfo =
+  WalletInfo
+    { wiReturnAddress :: Text
+    , wiAddress       :: Text
+    }
+
+instance ToJSON WalletInfo where
+  toJSON WalletInfo{wiReturnAddress, wiAddress} =
+    object
+      [ "return_address" .= wiReturnAddress
+      , "wallet_address" .= wiAddress
+      ]
+
+instance Pretty WalletInfo where
+  pretty WalletInfo{wiReturnAddress, wiAddress} =
+    vsep
+      [ "Return address:" <+> pretty wiReturnAddress
+      , "Wallet address:" <+> pretty wiAddress
+      ]
